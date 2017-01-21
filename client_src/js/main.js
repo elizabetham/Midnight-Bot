ef54cbeac195b6249e9035c028b16bd67f704bdc
Bodhi Mulders (BeMacized) <info@bemacized.net>
2017-01-21 02:15:14 +0100

    Fix material-ui styling

--- client_src/js/main.js
+++ client_src/js/main.js
@@ -4,6 +4,12 @@ import ReactDOM from 'react-dom';
 import Root from './layouts/Root.jsx';
 import {hashHistory} from 'react-router';
 
+//Normalize.css
+import 'normalize.css/normalize.css';
+
+//Font
+import 'roboto-npm-webfont';
+
 //React tap fix for material ui
 import injectTapEventPlugin from 'react-tap-event-plugin';
 injectTapEventPlugin();
