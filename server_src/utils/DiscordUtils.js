76503c1792f3562dde147c306bad4d0bce7f52a9
Bodhi Mulders (BeMacized) <info@bemacized.net>
2017-01-23 18:34:00 +0100

    Add game status persistence via Redis

--- server_src/utils/DiscordUtils.js
+++ server_src/utils/DiscordUtils.js
@@ -2,18 +2,25 @@
 
 import Config from '../../config';
 import {Client, Guild} from 'discord.js';
+import {Redis} from './DBManager';
 
 class DiscordUtils {
 
     client : Client;
     start : Function;
     stop : Function;
+    getPlaying : () => Promise < string >;
+    setPlaying : (game : string, save
+        ?
+        : boolean) => Promise < void >;
 
     //Constructor
     constructor() {
         this.client = new Client();
         this.start = this.start.bind(this);
         this.stop = this.stop.bind(this);
+        this.getPlaying = this.getPlaying.bind(this);
+        this.setPlaying = this.setPlaying.bind(this);
     }
 
     //Functions
@@ -39,6 +46,23 @@ class DiscordUtils {
         return await this.client.destroy();
     }
 
+    async getPlaying() : Promise < string > {
+        let res = await Redis.existsAsync("bot:game");
+        if (!res) {
+            return "";
+        }
+        return await Redis.getAsync("bot:game");
+    }
+
+    async setPlaying(game : string, save?: boolean = true) {
+        if (save) {
+            let res = await Redis.existsAsync("bot:game");
+            if (!res) {
+                Redis.set("bot:game", game);
+            }
+        }
+        this.client.user.setGame(game);
+    }
 }
 
 export default new DiscordUtils();
