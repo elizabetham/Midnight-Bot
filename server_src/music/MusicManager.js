bf92bc31b1833cf5216c91bff57e3c8946a9f4ef
Bodhi Mulders (BeMacized) <info@bemacized.net>
2017-02-01 01:25:59 +0100

    Better vote skipping

--- server_src/music/MusicManager.js
+++ server_src/music/MusicManager.js
@@ -206,9 +206,12 @@ class MusicManager {
         const votes = Array.from(this.votes.values()).length;
         const downvotes = Array.from(this.votes.values()).filter(vote => !vote).length;
         const upvotes = Array.from(this.votes.values()).filter(vote => vote).length;
+        const listeners = this.activeVoiceChannel
+            ? this.activeVoiceChannel.members.array().length - 1
+            : 0;
 
-        //25% negativity threshold;
-        if (downvotes / votes >= 0.50 && votes >= 5 && !this.skipped && event != "SONG_END") {
+        //Negativity threshold;
+        if (downvotes / listeners >= 0.50 && votes >= 5 && !this.skipped && event != "SONG_END") {
             //Block flow for effect
             this.skipped = true;
 
