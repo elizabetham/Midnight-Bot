c176ca6784a543100d2fab82000b6787e305bd15
Bodhi Mulders (BeMacized) <info@bemacized.net>
2017-02-04 01:37:31 +0100

    Disable fetchAllMembers

--- server_src/utils/DiscordUtils.js
+++ server_src/utils/DiscordUtils.js
@@ -25,7 +25,7 @@ class DiscordUtils {
 
     //Constructor
     constructor() {
-        this.client = new Client({fetchAllMembers: true});
+        this.client = new Client();
         this.start = this.start.bind(this);
         this.stop = this.stop.bind(this);
         this.getPlaying = this.getPlaying.bind(this);
